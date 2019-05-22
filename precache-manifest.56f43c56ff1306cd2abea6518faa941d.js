0f1ec329da02c4d3713bb7aa217d73e6284b976b
Nathan Griffiths <ngriffiths@postmedia.com>
2019-05-22 12:45:37 -0700

    forgot to add 'homepage' key to package.json. i THINK that's the issue

---
+++ precache-manifest.56f43c56ff1306cd2abea6518faa941d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "426b10beb9317f74e14e5de196b62b94",
+    "url": "/wildfire-tracker/index.html"
+  },
+  {
+    "revision": "337e73364290e9bf3dca",
+    "url": "/wildfire-tracker/static/css/main.61fc4ec5.chunk.css"
+  },
+  {
+    "revision": "f4a920894d879c59f5fb",
+    "url": "/wildfire-tracker/static/js/2.7b039c64.chunk.js"
+  },
+  {
+    "revision": "337e73364290e9bf3dca",
+    "url": "/wildfire-tracker/static/js/main.db0809d4.chunk.js"
+  },
+  {
+    "revision": "65bc327df9fcb13044f9",
+    "url": "/wildfire-tracker/static/js/runtime~main.7b17a158.js"
+  },
+  {
+    "revision": "6f6753fd733a216b24786a168657540a",
+    "url": "/wildfire-tracker/static/media/BentonSansCond-Bold.6f6753fd.otf"
+  },
+  {
+    "revision": "a6640d8951e3d6c523b2b58b0db9e351",
+    "url": "/wildfire-tracker/static/media/BentonSansCond-BoldItalic.a6640d89.otf"
+  },
+  {
+    "revision": "69a4217c7da7462a2511b1a8ecaefecf",
+    "url": "/wildfire-tracker/static/media/BentonSansCond-RegItalic.69a4217c.otf"
+  },
+  {
+    "revision": "43cd4ac75abf53e257d21366d3322b0e",
+    "url": "/wildfire-tracker/static/media/BentonSansCond-Regular.43cd4ac7.otf"
+  },
+  {
+    "revision": "cdd38a860699d029f95e76f21e51e216",
+    "url": "/wildfire-tracker/static/media/Shift-Bold.cdd38a86.otf"
+  },
+  {
+    "revision": "5c3edbfdeedc59bb35fac3bf1a461926",
+    "url": "/wildfire-tracker/static/media/Shift-BoldItalic.5c3edbfd.otf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e64562e4dd1382e6169b3ceb55665559.js"
+  "/wildfire-tracker/precache-manifest.56f43c56ff1306cd2abea6518faa941d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wildfire-tracker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
